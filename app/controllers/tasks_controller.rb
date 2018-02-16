87be0d9382631d2152cf7cec841a363524203a9a
Kate Catlin <kcatlin2@gmail.com>
2018-02-16 16:24:35 +0800

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,43 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new
+  end
+
+  def birth
+    task = Task.new(task_params)
+    task.save
+    redirect_to "/"
+  end
+
+  def update
+    @task = Task.find(params[:id])
+  end
+
+  def rebirth
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to "/"
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to "/"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'create', to: 'tasks#create', as: :create
+  get 'update/:id', to: 'tasks#update', as: :update
+  get 'delete/:id', to: 'tasks#delete', as: :delete
+  patch 'tasks/:id', to: 'tasks#rebirth', as: :rebirth
+  post 'tasks', to: 'tasks#birth', as: :birth
+
 end
